f32f91442ce5f7fa2f167669b107fdac6e76cec9
Dmitry Smirnov <divis1969@gmail.com>
2016-09-07 16:08:14 +0300

    Add Meizu PerfService libraries
    Change-Id: I97fa430221d9727e7069ec8ca0e4e6e0c8889950

--- meilan2-vendor-blobs.mk
+++ meilan2-vendor-blobs.mk
@@ -358,6 +358,7 @@ PRODUCT_COPY_FILES += \
     vendor/meizu/meilan2/proprietary/lib64/libui_ext.so:system/lib64/libui_ext.so \
     vendor/meizu/meilan2/proprietary/lib/libui.so:system/lib/libui.so \
     vendor/meizu/meilan2/proprietary/lib64/libui.so:system/lib64/libui.so \
+    vendor/meizu/meilan2/proprietary/etc/perfservscntbl.txt:system/etc/perfservscntbl.txt \
     vendor/meizu/meilan2/proprietary/usr/keylayout/ACCDET.kl:system/usr/keylayout/ACCDET.kl \
     vendor/meizu/meilan2/proprietary/usr/keylayout/AW9201_ts.kl:system/usr/keylayout/AW9201_ts.kl \
     vendor/meizu/meilan2/proprietary/usr/keylayout/mtk-kpd.kl:system/usr/keylayout/mtk-kpd.kl \
--- prebuilt/libs/Android.mk
+++ prebuilt/libs/Android.mk
@@ -52,3 +52,13 @@ LOCAL_SRC_FILES_32  := $(PROPRIETARY_PATH)/lib/libmeizucamera.so
 LOCAL_MULTILIB := both
 include $(BUILD_PREBUILT)
 
+include $(CLEAR_VARS)
+LOCAL_MODULE        := libperfservice
+LOCAL_MODULE_TAGS   := optional
+LOCAL_MODULE_CLASS  := SHARED_LIBRARIES
+LOCAL_MODULE_SUFFIX := .so
+LOCAL_SRC_FILES_64  := $(PROPRIETARY_PATH)/lib64/libperfservice.so
+LOCAL_SRC_FILES_32  := $(PROPRIETARY_PATH)/lib/libperfservice.so
+LOCAL_MULTILIB := both
+include $(BUILD_PREBUILT)
+
