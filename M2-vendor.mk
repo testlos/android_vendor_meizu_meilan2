118c7ef7b5937d38c2b73d340e852ba764cc37f0
adi766 <adityakumarteli7846@gmail.com>
2017-10-06 13:54:25 +0000

    Add Override files

---
+++ M2-vendor.mk
@@ -0,0 +1,8 @@
+PRODUCT_COPY_FILES += $(call find-copy-subdir-files,*,vendor/meizu/meilan2/overrides/app,system/app)
+
+PRODUCT_COPY_FILES += $(call find-copy-subdir-files,*,vendor/meizu/meilan2/overrides/etc,system/etc)
+
+PRODUCT_COPY_FILES += $(call find-copy-subdir-files,*,vendor/meizu/meilan2/overrides/lib,system/lib)
+
+PRODUCT_COPY_FILES += $(call find-copy-subdir-files,*,vendor/meizu/meilan2/overrides/lib64,system/lib64)
+
